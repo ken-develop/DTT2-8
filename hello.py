3f083e54d07ff7d7ea33882b1ba47c040bda5412
Luk2244 <luckyvalron@gmail.com>
2026-09-03 13:30:56 +0700

    poramet

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 print('hello git')
-num1= int(input(input number 1:))
-num2= int(input(input number 2:))
+num1= int(input('input number 1:'))
+num2= int(input('input number 2:'))
 result=num1+num2
 prunt('Result:',result)
